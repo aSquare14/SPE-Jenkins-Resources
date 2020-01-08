a7b393ad5a955b284c659c47716cb7acd865152f
Atibhi Agrawal <atibhi.a@gmail.com>
2020-01-08 12:33:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 echo deb http://pkg.jenkins.io/debian-stable binary/ | \ sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
 sudo apt-get install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
